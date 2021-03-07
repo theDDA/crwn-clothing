688017cd164ac87693ce7c14c361beb8aa944645
theDDA <damir.agacevic@gmail.com>
2021-03-07 13:44:07 +0100

    Sign up and sign in is now fully functional.

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
             },
           });
 
-          console.log(this.state);
+          //console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
